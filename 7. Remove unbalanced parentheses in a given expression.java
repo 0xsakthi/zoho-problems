25d19dfd874c292eb47882b6953a44bbfab1cd6f
Sakthi <82046832+0xsakthi@users.noreply.github.com>
2022-07-27 18:02:43 +0530

    7

--- 7. Remove unbalanced parentheses in a given expression.java
+++ 7. Remove unbalanced parentheses in a given expression.java
@@ -4,6 +4,8 @@
 // Output : ((abc)(de))
 // Input : (((ab)
 // Output : (ab)
+
+// METHOD 1 SOMETHING WANT TO CHANGE
 import java.util.Collection;
 public class Main {
     public static void main(String[] args) {
@@ -39,4 +41,48 @@ public class Main {
         }
         System.out.print(ans); //(ab)
     }
+}
+
+// METHOD 2 PASS
+
+// "static void main" must be defined in a public class.
+// 7. Remove unbalanced parentheses in a given expression.
+// Eg.) Input : ((abc)((de))
+// Output : ((abc)(de))
+// Input : (((ab)
+// Output : (ab)
+//answer->https://www.youtube.com/watch?v=nDPWB7oQOw8
+// open ans close bracket count edukanum open or close -1 pona antha bracket theva ilama iruku so print pana theva ila
+import java.util.Collection;
+public class Main {
+    public static void main(String[] args) {
+        String s = "(((ab)";
+        char[] c = s.toCharArray();
+        int i = 0;
+        int j = s.length()-1;
+        int open = 0;
+        int close = 0;
+        while(i<s.length()){
+            if(c[i]=='(') open++;
+            else if(c[i]==')') open--;
+            
+            if(c[j]==')') close++;
+            else if(c[j]=='(') close--;
+            
+            if(open<0) {
+                c[i]='1';
+                open = 0;
+            }
+            if(close<0){
+                c[j]='1';
+                close=0;
+                }
+            i++;
+            j--;
+        }
+        for(i=0;i<c.length;i++){
+            if(c[i]!='1') System.out.print(c[i]);
+        }
+    }
+    
 }
